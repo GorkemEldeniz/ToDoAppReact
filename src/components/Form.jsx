1351fbe7d15244914086f81a098bef08e1febb32
GorkemEldeniz <gorkemeldeniz30@gmail.com>
2022-06-27 11:32:43 +0300

    edit kısmı router ile yapıldıç...

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,23 +4,21 @@ import {useContextCustom} from '../Context/index'
 function Form() {
 
   const {todos,setTodos} = useContextCustom()  
+  const [input,setInput] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const data =  {id : todos.length , time : Date.now(),checked:false, ...Object.fromEntries(new FormData(e.target))}
+    const data =  {id : todos.length , time : Date.now(),checked:false, task : input , edit:false}
     setTodos([...todos,data])
+    setInput('')
   }     
 
-  useEffect(() => {
-    console.log(todos);
-  },[todos])
 
   return (
     <form onSubmit={handleSubmit}>
-    <input type="text" id="task" name="task"  maxlength="20" placeholder="ToDo..." autocomplete="off" />
+    <input type="text" id="task" name="task"  value={input} onChange={e => setInput(e.target.value)} maxLength="20" placeholder="ToDo..." autoComplete='off'/>
     <button type="submit"><i className="fa-solid fa-plus"></i></button> 
-</form>
-
+    </form>
   )
 }
 
